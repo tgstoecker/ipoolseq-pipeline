7363d1c9bab82eb99a67814663fb350ed790b86b
Florian G. Pflug <florian.pflug@univie.ac.at>
2019-03-02 17:14:25 +0100

    Added and updated copyright notices

--- scripts/simulate_trumicount_output.R
+++ scripts/simulate_trumicount_output.R
@@ -1,3 +1,21 @@
+# simulate_trumicount_output.R, Copyright 2018, 2019 Florian G. Pflug
+# 
+# This file is part of the iPool-Seq Analysis Pipeline
+#
+# The iPool-Seq Analysis Pipeline is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# The iPool-Seq Analysis Pipeline is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with the iPool-Seq Analysis Pipeline.  If not, see
+# <http://www.gnu.org/licenses/
+
 library(data.table)
 library(rtracklayer)
 library(gwpcR)
