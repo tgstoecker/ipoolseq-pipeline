1275fe3a00aff1ad90f1cd88ba70713c84666b9e
Florian G. Pflug <florian.pflug@univie.ac.at>
2019-03-07 15:39:52 +0100

    Removed outdated debugging help from ipoolseq.differential.virulence.R

--- scripts/ipoolseq.differential.virulence.R
+++ scripts/ipoolseq.differential.virulence.R
@@ -18,19 +18,7 @@
 
 library(rmarkdown)
 
-if (exists("SNAKEMAKE.STANDIN") && get("SNAKEMAKE.STANDIN")) {
-  setClass("SnakemakeStandin", representation(input = "list", output = "list"))
-  
-  snakemake <- new("SnakemakeStandin",
-    input=list(`in`="data/Simulation/sim-in.count.tab",
-               `out`="data/Simulation/sim-out.count.tab",
-               `knockouts`="cfg/Uhse_et_al.2018/knockouts.gff"),
-    output=list(`table`="data/Simulation/sim.da.tab",
-                `html`="data/Simulation/sim.report.html")
-  )
-}
-
-rmarkdown::render(input="scripts/differential_virulence.Rmd",
+rmarkdown::render(input="scripts/ipoolseq.differential.virulence.Rmd",
                   output_file=basename(snakemake@output$`html`),
                   output_dir=normalizePath(dirname(snakemake@output$`html`)),
                   output_format="html_document",
