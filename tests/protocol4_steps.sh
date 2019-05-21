606e74819c8d2dc97ff8ffda2735dcff2681bc20
Florian G. Pflug <florian.pflug@univie.ac.at>
2019-05-21 16:29:10 +0200

    Updated protocol4_steps.sh to reflect the new installation instructions

--- tests/protocol4_steps.sh
+++ tests/protocol4_steps.sh
@@ -1,49 +1,52 @@
 #!/bin/bash
-# Enable & update conda
-. /opt/conda/etc/profile.d/conda.sh
-conda update -n base -c defaults conda
-export USER=user
-
-# 1a. Download pipeline
+# Download and unpack the pipeline
+# 1.
 VER=latest-release
 URL=http://github.com/Cibiv/ipoolseq-pipeline/archive
-curl -L -O $URL/$VER.zip
-unzip $VER.zip
-cd ipoolseq-pipeline-$VER || exit 1
+curl -L -O $URL/$VER.tar.gz
+tar xzf $VER.tar.gz
+cd ipoolseq-pipeline-$VER
 
-# 1b. Create & activate environment
-conda env create --file ipoolseq.yaml || exit 1
-conda activate ipoolseq
+# Install & activate environment
+# 2.
+./install-environment.sh
+source ./environment/bin/activate
 
-# No errors now
-set -e
-set -p pipefail
+# Test
+# 3.
+snakemake data/Uhse_et_al.2018/expA.r1.dv.tab
 
-# 2. Add reference, knockouts, libraries
+# Add reference, knockouts, libraries
+# 4.
 mkdir -p data/your_design
-mkdir cfg/your_design
+mkdir -p cfg/your_design
+# 5.
 cp cfg/Uhse_et_al.2018/reference.fa cfg/your_design/reference.fa
 cp cfg/Uhse_et_al.2018/knockouts.gff cfg/your_design/knockouts.gff
 cp cfg/Uhse_et_al.2018/cassette.fa cfg/your_design/cassette.fa
+# 6.
 curl -o data/your_design/your_replicate-in.bam ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR219/ERR2190337/r4896/in1.bam
 curl -o data/your_design/your_replicate-out.bam ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR219/ERR2190334/r4896/egb73r1.bam 
 
-# 3. Trim
+# Trim
+# 7.
 snakemake data/your_design/your_replicate-in.trim.1.fq.gz
+snakemake data/your_design/your_replicate-out.trim.1.fq.gz
+# 8.
 snakemake data/your_design/your_replicate-in.fastqc.1.html
 snakemake data/your_design/your_replicate-in.fastqc.2.html
-
-snakemake data/your_design/your_replicate-out.trim.1.fq.gz
 snakemake data/your_design/your_replicate-out.fastqc.1.html
 snakemake data/your_design/your_replicate-out.fastqc.2.html
 
-# 4. Map & assign
-snakemake data/your_design/your_replicate-in.assign.bam
+# Map & assign
+# 9.
 snakemake data/your_design/your_replicate-out.assign.bam
 
-# 5. Abundance
+# Abundance
+# 10.
 snakemake data/your_design/your_replicate-in.count.tab
 snakemake data/your_design/your_replicate-out.count.tab
 
-# 7. Differential virulence
+# Differential virulence
+# 13.
 snakemake data/your_design/your_replicate.dv.tab
