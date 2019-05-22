9ff7f4f39926b4b82b04890a668ca4cc2faf0fff
Florian G. Pflug <florian.pflug@univie.ac.at>
2019-05-22 15:21:24 +0200

    Retry downloading environment.tar.gz if the checksum is wrong after downloading

--- install-environment.sh
+++ install-environment.sh
@@ -14,22 +14,37 @@ if [ ! -e "environment.tar.gz" ] || (head -n1 "environment.tar.gz" | grep '^vers
 	# correctly, and redirect us to the actual storage site.
 	echo "*** environment.tar.gz is only a git LFS pointer, downloading original from GitHub"
 	URL="http://github.com/Cibiv/ipoolseq-pipeline/raw/$(cat environment.rev)/environment.tar.gz"
-	if [ -x "$(command -v curl)" ]; then
-		curl -O -L "$URL" || exit 1
-	elif [ -x "$(command -v wget)" ]; then
-		wget "$URL" || exit 1
+	while true; do
+		if [ -x "$(command -v curl)" ]; then
+			curl -O -L "$URL" || exit 1
+		elif [ -x "$(command -v wget)" ]; then
+			wget "$URL" || exit 1
+		else
+			echo "Neither curl nor wget are installed, one of these is required to download the environment" >&2
+			exit 1
+		fi
+
+		if [ -x "$(command -v sha256sum)" ]; then
+			echo "*** Verifying the integrity of environment.tar.gz"
+			if ! sha256sum -c environment.sha256; then
+				echo "*** Downloaded environment.tar.gz is corrupt, will remove and retry"
+				rm environment.tar.gz
+				continue
+			fi
+		else
+			echo "*** sha256sum is not available, cannot verify environment.tar.gz, trusting that the download was OK"
+		fi
+		break
+	done
+else
+	if [ -x "$(command -v sha256sum)" ]; then
+		echo "*** Verifying the integrity of environment.tar.gz"
+		sha256sum -c environment.sha256 || exit 1
 	else
-		echo "Neither curl nor wget are installed, one of these is required to download the environment" >&2
-		exit 1
+		echo "*** sha256sum is not available, cannot verify environment.tar.gz"
 	fi
 fi
 
-if [ -x "$(command -v sha256sum)" ]; then
-	echo "*** Verifying the integrity of environment.tar.gz"
-	sha256sum -c environment.sha256 || exit 1
-else
-	echo "*** sha256sum is not available, cannot verify environment.tar.gz"
-fi
 
 echo "*** Unpacking environment.tar.gz"
 test -e ./environment || mkdir ./environment || exit 1
